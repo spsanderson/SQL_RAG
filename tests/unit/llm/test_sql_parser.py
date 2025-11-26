f66c9595271f895d28f38607c811aeb4cfd9900a
Steven Paul Sanderson II, MPH <spsanderson@users.noreply.github.com>
2025-11-26 16:58:04 -0500

    Update test_sql_parser.py

--- tests/unit/llm/test_sql_parser.py
+++ tests/unit/llm/test_sql_parser.py
@@ -1,51 +1,30 @@
 """
-Test SQL Parser
+Unit tests for SQLParser.
 """
 import pytest
 from src.llm.sql_parser import SQLParser
 
-def test_parse_extracts_sql_block():
-    """
-    Test extracting SQL from a markdown code block.
-    """
+def test_parse_markdown_sql():
     parser = SQLParser()
-    response = """
-Here is the query:
-```sql
-SELECT * FROM users
-```
-Hope that helps!
-"""
-    sql = parser.parse(response)
-    assert sql == "SELECT * FROM users"
+    response = "Here is the query:\n```sql\nSELECT * FROM table\n```"
+    assert parser.parse(response) == "SELECT * FROM table"
 
-def test_parse_extracts_generic_block():
-    """
-    Test extracting SQL from a generic code block.
-    """
+def test_parse_markdown_generic():
     parser = SQLParser()
-    response = """
-```
-SELECT * FROM users
-```
-"""
-    sql = parser.parse(response)
-    assert sql == "SELECT * FROM users"
+    response = "```\nSELECT * FROM table\n```"
+    assert parser.parse(response) == "SELECT * FROM table"
 
-def test_parse_extracts_raw_text():
-    """
-    Test extracting SQL from raw text.
-    """
+def test_parse_raw():
     parser = SQLParser()
-    response = "SELECT * FROM users"
-    sql = parser.parse(response)
-    assert sql == "SELECT * FROM users"
+    response = "SELECT * FROM table"
+    assert parser.parse(response) == "SELECT * FROM table"
 
-def test_parse_removes_prefix():
-    """
-    Test removing 'SQL Query:' prefix.
-    """
+def test_parse_prefix():
     parser = SQLParser()
-    response = "SQL Query: SELECT * FROM users"
-    sql = parser.parse(response)
-    assert sql == "SELECT * FROM users"
+    response = "SQL Query: SELECT * FROM table"
+    assert parser.parse(response) == "SELECT * FROM table"
+
+def test_parse_tags():
+    parser = SQLParser()
+    response = "<start>SELECT * FROM table<end>"
+    assert parser.parse(response) == "SELECT * FROM table"
